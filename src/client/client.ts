fee6ce7fec236de354105ea8fbd83303ebd4049c
Jarrod Medrano <jmedran@gmail.com>
2022-05-22 17:59:05 -0500

    work

--- src/client/client.ts
+++ src/client/client.ts
@@ -46,6 +46,11 @@ function onWindowResize() {
 }
 
 const gui = new GUI();
+const cubeRotationFolder = gui.addFolder("Cube");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeRotationFolder.open();
 const cubeFolder = gui.addFolder("Cube");
 cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
 cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
